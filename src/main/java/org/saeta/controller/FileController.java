d2fc9b4e15d44eb08e0dcabd4afbed87c5aff3fa
Athny710 <a20145976@pucp.pe>
2022-08-03 11:58:43 -0500

    Upload files

--- src/main/java/org/saeta/controller/FileController.java
+++ src/main/java/org/saeta/controller/FileController.java
@@ -1,9 +1,13 @@
 package org.saeta.controller;
 
+import org.saeta.service.file.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 @RestController
@@ -11,13 +15,22 @@ import java.util.HashMap;
 @RequestMapping("files")
 public class FileController {
 
+    @Autowired
+    FileService fileService;
+
     @ResponseBody
-    @GetMapping(value = "test")
-    public ResponseEntity prueba(){
+    @PostMapping("/upload")
+    public ResponseEntity uploadFile(@RequestParam("file")MultipartFile file){
         HashMap<String, Object> responseMap = new HashMap<>();
-
-        responseMap.put("estado", "Works!");
-
+        try{
+            fileService.deleteFile(file.getOriginalFilename());
+        }catch (IOException e){
+            System.out.println("No existe el archivo"   );
+        }
+        fileService.save(file);
+        responseMap.put("status", "ok");
+        responseMap.put("fileName", file.getOriginalFilename());
         return new ResponseEntity(responseMap, HttpStatus.OK);
+
     }
 }
